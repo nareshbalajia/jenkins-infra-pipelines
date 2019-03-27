94addbba24ab4b6fe82fa358592db206aa17f172
Naresh <na@logitech.com>
2019-03-27 14:34:59 +0530

    Adding pipeline definition for vpc pipeline

---
+++ vpc-deployment/vpc_deployment.groovy
@@ -0,0 +1,23 @@
+tf_git_url = "git@github.com:nareshbalajia/awsvpc-tf-module.git"
+tf_git_branch = "master"
+job_name = "deploy-vpc-infra"
+job_path = "${new File(__FILE__).parent}"
+jenkinsfile = job_path + "/Jenkinsfile"
+
+pipelineJob(job_name) {
+  description("Jenkins pipeline to deploy VPC infrastructure")
+  logRotator(-1,100)
+  parameters {
+    stringParam("TF_GIT_URL", tf_git_url, "GIT URL of repo containing Terraform template for infrastructure")
+    stringParam("TF_GIT_BRANCH", tf_git_branch, "GIT Branch of repo containing cf stack for infrastructure")
+    stringParam("TF_STATE_BUCKET", "", "The S3 bucket name where TF state is stored")
+    stringParam("REGION", REGION, "Target region where this stack needs to be deployed")
+    choiceParam("OPERATION", ["create/update", "destroy"], "Terraform operation to perform")
+  }
+  definition {
+    cps {
+      sandbox()
+      script(readFileFromWorkspace(jenkinsfile))
+    }
+  }
+}
