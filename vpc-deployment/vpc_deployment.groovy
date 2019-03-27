0f53d76e5e59dd66ef04e81833f40bca8e725328
Naresh <na@logitech.com>
2019-03-27 15:53:12 +0530

    Adding region param

--- vpc-deployment/vpc_deployment.groovy
+++ vpc-deployment/vpc_deployment.groovy
@@ -3,6 +3,7 @@ tf_git_branch = "master"
 job_name = "deploy-vpc-infra"
 job_path = "${new File(__FILE__).parent}"
 jenkinsfile = job_path + "/Jenkinsfile"
+REGION = "us-east-1"
 
 pipelineJob(job_name) {
   description("Jenkins pipeline to deploy VPC infrastructure")
@@ -10,7 +11,7 @@ pipelineJob(job_name) {
   parameters {
     stringParam("TF_GIT_URL", tf_git_url, "GIT URL of repo containing Terraform template for infrastructure")
     stringParam("TF_GIT_BRANCH", tf_git_branch, "GIT Branch of repo containing cf stack for infrastructure")
-    stringParam("TF_STATE_BUCKET", "", "The S3 bucket name where TF state is stored")
+    stringParam("TF_STATE_BUCKET", "jenkins-terraform-s3-bucket", "The S3 bucket name where TF state is stored")
     stringParam("REGION", REGION, "Target region where this stack needs to be deployed")
     choiceParam("OPERATION", ["create/update", "destroy"], "Terraform operation to perform")
   }
